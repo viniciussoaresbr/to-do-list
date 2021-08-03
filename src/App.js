2531629389ada994b669ff25943b998c3cf3747b
Vinicius Soares <viniciussoaresmbr@gmail.com>
2021-08-03 16:05:34 -0300

    Criado NoteList.jsx

--- src/App.js
+++ src/App.js
@@ -2,26 +2,30 @@ import { useState } from "react";
 import "./App.css";
 import Form from "./components/form/Form";
 import Header from "./components/header/Header";
+import NoteList from "./components/notelist/NoteList";
 
 function App() {
-    const [showForm, setShowForm] = useState(false);
+  const [showForm, setShowForm] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const fnToggleDone = (index) => {};
 
-    return (
-        <div className="App">
-            <Header
-                fnToggleForm={() => {
-                    setShowForm(!showForm);
-                }}
-            />
-            {showForm ? (
-                <Form
-                    evSubmit={({ text }) => {
-                        console.log(text);
-                    }}
-                />
-            ) : null}
-        </div>
-    );
+  return (
+    <div className="App">
+      <Header
+        fnToggleForm={() => {
+          setShowForm(!showForm);
+        }}
+      />
+      {showForm ? (
+        <Form
+          evSubmit={({ text }) => {
+            setNotes([...notes, { text, done: false }]);
+          }}
+        />
+      ) : null}
+      <NoteList notes={notes} fnToggleDone={fnToggleDone} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/notelist/NoteList.jsx
@@ -0,0 +1,18 @@
+const NoteList = ({ notes, fnToggleDone }) => {
+  return (
+    <div>
+      {notes.map((note, index) => {
+        return (
+          <Note
+            note={note}
+            key={index}
+            index={index}
+            fnToggleDone={fnToggleDone}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default NoteList;
