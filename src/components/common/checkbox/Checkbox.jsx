3653e86a446a971034fafba216244bf8d45c351c
Bruno <brunorsousa1998@gmail.com>
2021-08-09 16:34:11 -0300

    Arrumado checkbox

--- src/components/common/checkbox/Checkbox.jsx
+++ src/components/common/checkbox/Checkbox.jsx
@@ -1,15 +1,12 @@
 import { useState } from "react";
 import "./Checkbox.css";
 
-const Checkbox = ({ evClick = () => {} }) => {
-    const [checked, setChecked] = useState(false);
-
+const Checkbox = ({ evClick = () => {}, checked = false }) => {
     return (
         <div
             className="checkbox"
             onClick={() => {
                 evClick({ checked: !checked });
-                setChecked(!checked);
             }}
         >
             <div className={checked ? "checkbox--checked" : ""}></div>
