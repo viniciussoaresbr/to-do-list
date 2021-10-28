824114912e869451c57d064e59caa20f90f8a1a8
Vinicius Soares <viniciussoaresmbr@gmail.com>
2021-10-28 11:17:11 -0300

    refatorando código do App.js

--- src/App.js
+++ src/App.js
@@ -3,50 +3,24 @@ import "./App.css";
 import Form from "./components/form/Form";
 import Header from "./components/header/Header";
 import NoteList from "./components/notelist/NoteList";
+import NotesContextProvider from "./contexts/NotesContext";
 
 function App() {
-    const [showForm, setShowForm] = useState(false);
-    const [notes, setNotes] = useState([]);
-    const [searchStr, setSearchStr] = useState("");
-    const [done, setDone] = useState(true);
-    const [undone, setUndone] = useState(true);
+  const [showForm, setShowForm] = useState(false);
 
-    return (
-        <div className="App">
-            <Header
-                fnToggleForm={() => {
-                    setShowForm(!showForm);
-                }}
-            />
-            {showForm ? (
-                <Form
-                    evSubmit={({ text }) => {
-                        setNotes([...notes, { text, done: false }]);
-                    }}
-                    searchStr={searchStr}
-                    fnChangeSearchStr={({ value }) => {
-                        setSearchStr(value);
-                    }}
-                    done={done}
-                    fnChangeDone={({ checked }) => {
-                        setDone(checked);
-                    }}
-                    undone={undone}
-                    fnChangeUndone={({ checked }) => {
-                        setUndone(checked);
-                    }}
-                />
-            ) : null}
-            <NoteList
-                notes={notes}
-                fnToggleDone={(index, checked) => {
-                    const attNotes = notes;
-                    attNotes[index].done = checked;
-                    setNotes(attNotes);
-                }}
-            />
-        </div>
-    );
+  return (
+    <div className="App">
+      <Header
+        fnToggleForm={() => {
+          setShowForm(!showForm);
+        }}
+      />
+      <NotesContextProvider>
+        {showForm ? <Form /> : null}
+        <NoteList />
+      </NotesContextProvider>
+    </div>
+  );
 }
 
 export default App;
