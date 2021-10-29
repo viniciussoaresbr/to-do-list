9487bd961b8bd5010862a2dc38aab6415dc0e186
Vinicius Soares <viniciussoaresmbr@gmail.com>
2021-10-28 23:02:34 -0300

    criando componente Main.jsx

---
+++ src/components/main/Main.jsx
@@ -0,0 +1,14 @@
+import Form from "../form/Form";
+import Statistics from "../statistics/Statistics";
+import "./Main.css";
+
+const Main = () => {
+  return (
+    <main className="main">
+      <Form />
+      <Statistics />
+    </main>
+  );
+};
+
+export default Main;
