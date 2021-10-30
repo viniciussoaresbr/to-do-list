498eaf405ab2aecc164f771cd1c59c9d15ec7de6
Vinicius Soares <viniciussoaresmbr@gmail.com>
2021-10-30 16:51:44 -0300

    corrigindo import do useContext

--- src/components/notelist/NoteList.jsx
+++ src/components/notelist/NoteList.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react/cjs/react.development";
+import { useContext } from "react";
 import { NotesContext } from "../../contexts/NotesContext";
 import Note from "../note/Note";
 import "./NoteList.css";
