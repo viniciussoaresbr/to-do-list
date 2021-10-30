89996a03e1cd352f9ae144914b3568ad8ba86f19
Vinicius Soares <viniciussoaresmbr@gmail.com>
2021-10-30 15:06:42 -0300

    adicionando evento responsável por remover a nota

--- src/components/note/Note.jsx
+++ src/components/note/Note.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useContext } from "react/cjs/react.development";
 import { NotesContext } from "../../contexts/NotesContext";
 import Checkbox from "../common/checkbox/Checkbox";
@@ -5,11 +6,28 @@ import Checkbox from "../common/checkbox/Checkbox";
 import "./Note.css";
 
 const Note = ({ note = { text: "Note text", done: false, id: 0 } }) => {
-  const { fnToggleDone } = useContext(NotesContext);
+  const { fnToggleDone, removeNote } = useContext(NotesContext);
+  const [showAdvice, setShowAdvice] = useState(false);
 
   return (
-    <div className="note">
-      <span className="note__text">{note.text}</span>
+    <div
+      className="note"
+      onMouseOver={() => {
+        setShowAdvice(true);
+      }}
+      onMouseOut={() => {
+        setShowAdvice(false);
+      }}
+      onDoubleClick={() => {
+        removeNote(note.id);
+      }}
+    >
+      <div className="note__container">
+        <span className="note__advice">
+          {showAdvice && "* Clique duas vezes para excluir"}
+        </span>
+        <span className="note__text">{note.text}</span>
+      </div>
       <Checkbox
         checked={note.done}
         evClick={({ checked }) => {
