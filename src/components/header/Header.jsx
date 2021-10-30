59beedc55a3a45bac34254eaa968d51b950bb14f
Vinicius Soares <viniciussoaresmbr@gmail.com>
2021-10-30 15:04:44 -0300

    alterando texto do button dinamicamente

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
 import Button from "../common/button/Button";
 import "./Header.css";
 
 const Header = ({ fnToggleForm }) => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <header className="header">
       <h1 className="header__logo">To-do list</h1>
       <Button
-        text="Menu"
+        text={toggleMenu ? "Fechar Menu" : "Abrir Menu"}
         evClick={() => {
+          setToggleMenu(!toggleMenu);
           fnToggleForm();
         }}
       />
